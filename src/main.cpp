f4d73f0d825b7ad09d8e383ecff8319b62cfc40e
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2024-03-01 01:00:27 +0000

    Format code

--- src/main.cpp
+++ src/main.cpp
@@ -9,89 +9,23 @@ U8G2_SSD1305_128X32_ADAFRUIT_F_HW_I2C u8g2(U8G2_R0);
 
 volatile uint32_t currentStepSize = 0;
 
-// Step sizes for each note
-const uint32_t stepSizes[] = {
-    51076057, 54113197, 57330935, 60740010, 64351799, 68178356,
-    72232452, 76527617, 81078186, 85899346, 91007187, 96418756,
-    102152113, 108226394, 114661870, 121480020, 128703598, 136356712,
-    144464904, 153055234, 162156372, 171798692, 182014374, 192837512,
-    204304227, 216452788, 229323741, 242960040, 257407196, 272713424,
-    288929808, 306110469, 324312744, 343597384, 364028747, 385675023
-    };
+// Phase accumulator increment sizes for each note
+const uint32_t stepSizes[] = {51076057,  54113197,  57330935,  60740010,  64351799,  68178356,  72232452,  76527617,
+                              81078186,  85899346,  91007187,  96418756,  102152113, 108226394, 114661870, 121480020,
+                              128703598, 136356712, 144464904, 153055234, 162156372, 171798692, 182014374, 192837512,
+                              204304227, 216452788, 229323741, 242960040, 257407196, 272713424, 288929808, 306110469,
+                              324312744, 343597384, 364028747, 385675023};
 
-// Notes for each step
-const char *note_name[] = {
-    "None","C", "C#", "D", "D#", "E", "F",
-    "F#", "G", "G#", "A", "A#", "B"};
+const std::array<std::string, 12> noteNames = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
-// System state
-struct
-{// input state
-  std::bitset<32> inputs;
-  // note state
-  std::uint32_t note;
-  // rotation state
-  std::uint32_t rotationVariable;
-  // mutex
-  SemaphoreHandle_t mutex;  
+struct {
+    SemaphoreHandle_t mutex;
+    std::bitset<32> keys; // input state
+    std::array<int32_t, 4> knobPos; // rotation state
+    std::optional<uint8_t> noteIdx; // note state
 } sysState;
 
-
-uint8_t highestBitSet(std::bitset<12> bits)
-{
-  for (int i = 11; i >= 0; i--)
-  {
-    if (bits.test(i))
-    {
-      return i;
-    }
-  }
-  return 0;
-}
-
-// Function to set outputs using key matrix
-void setOutMuxBit(const uint8_t bitIdx, const bool value)
-{
-  digitalWrite(WEN_PIN, LOW);
-  digitalWrite(RA0_PIN, bitIdx & 0x01);
-  digitalWrite(RA1_PIN, bitIdx & 0x02);
-  digitalWrite(RA2_PIN, bitIdx & 0x04);
-  digitalWrite(OUT_PIN, value);
-  digitalWrite(WEN_PIN, HIGH);
-  delayMicroseconds(2);
-  digitalWrite(WEN_PIN, LOW);
-}
-
-// Function to read inputs using key matrix
-std::bitset<4> readCols()
-{
-  std::bitset<4> result;
-
-  // Read the inputs
-  result[0] = digitalRead(C0_PIN);
-  result[1] = digitalRead(C1_PIN);
-  result[2] = digitalRead(C2_PIN);
-  result[3] = digitalRead(C3_PIN);
-
-  return result;
-}
-
-// Function to select a given row of the switch matrix
-void setRow(uint8_t rowIdx)
-{
-  // Disable row select enable
-  digitalWrite(WEN_PIN, LOW);
-
-  // Set row select address pins
-  digitalWrite(RA0_PIN, rowIdx & 0x01);
-  digitalWrite(RA1_PIN, rowIdx & 0x02);
-  digitalWrite(RA2_PIN, rowIdx & 0x04);
-
-  // Enable row select enable
-  digitalWrite(WEN_PIN, HIGH);
-}
-
-// Function to update the phase accumulator and set the analogue output voltage at each sample interval
+// Function to update the phase accumulator and set the analogue output voltage
 void sampleISR()
 {
   static uint32_t phaseAcc = 0;
@@ -255,31 +189,29 @@ void displayUpdateTask(void *pvParameters)
 
 void setup()
 {
-  // put your setup code here, to run once:
+    pinMode(RA0_PIN, OUTPUT);
+    pinMode(RA1_PIN, OUTPUT);
+    pinMode(RA2_PIN, OUTPUT);
+    pinMode(WEN_PIN, OUTPUT);
+    pinMode(OUT_PIN, OUTPUT);
+    pinMode(OUTL_PIN, OUTPUT);
+    pinMode(OUTR_PIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  // Set pin directions
-  pinMode(RA0_PIN, OUTPUT);
-  pinMode(RA1_PIN, OUTPUT);
-  pinMode(RA2_PIN, OUTPUT);
-  pinMode(WEN_PIN, OUTPUT);
-  pinMode(OUT_PIN, OUTPUT);
-  pinMode(OUTL_PIN, OUTPUT);
-  pinMode(OUTR_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(C0_PIN, INPUT);
+    pinMode(C1_PIN, INPUT);
+    pinMode(C2_PIN, INPUT);
+    pinMode(C3_PIN, INPUT);
+    pinMode(JOYX_PIN, INPUT);
+    pinMode(JOYY_PIN, INPUT);
 
-  pinMode(C0_PIN, INPUT);
-  pinMode(C1_PIN, INPUT);
-  pinMode(C2_PIN, INPUT);
-  pinMode(C3_PIN, INPUT);
-  pinMode(JOYX_PIN, INPUT);
-  pinMode(JOYY_PIN, INPUT);
-
-  // Initialise display
-  setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
-  delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
-  u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
+    // Initialise display
+    setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
+    delayMicroseconds(2);
+    setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
+    setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
+    u8g2.begin();
+    u8g2.setContrast(0);
 
     // Initialise UART
     Serial.begin(115200);
@@ -299,38 +231,35 @@ void setup()
   sampleTimer->attachInterrupt(sampleISR);
   sampleTimer->resume();
 
-  // Mutex handle
-  sysState.mutex = xSemaphoreCreateMutex();
+    // Mutex handle
+    sysState.mutex = xSemaphoreCreateMutex();
 
-  // Set up task handles
-  TaskHandle_t scanKeysHandle = NULL;
-  xTaskCreate(
-      scanKeysTask, /* Function that implements the task */
-      "scanKeys",   /* Text name for the task */
-      64,           /* Stack size in words, not bytes */
-      NULL,         /* Parameter passed into the task */
-      2,            /* Task priority */
-      &scanKeysHandle);
+    // Set up task handles
+    TaskHandle_t scanKeysHandle = nullptr;
+    xTaskCreate(scanKeysTask, /* Function that implements the task */
+                "scanKeys", /* Text name for the task */
+                64, /* Stack size in words, not bytes */
+                NULL, /* Parameter passed into the task */
+                2, /* Task priority */
+                &scanKeysHandle);
 
-  TaskHandle_t displayUpdateHandle = NULL;
-  xTaskCreate(
-      displayUpdateTask, /* Function that implements the task */
-      "displayUpdate",   /* Text name for the task */
-      256,               /* Stack size in words, not bytes */
-      NULL,              /* Parameter passed into the task */
-      1,                 /* Task priority */
-      &displayUpdateHandle);
+    TaskHandle_t displayUpdateHandle = nullptr;
+    xTaskCreate(displayUpdateTask, /* Function that implements the task */
+                "displayUpdate", /* Text name for the task */
+                256, /* Stack size in words, not bytes */
+                NULL, /* Parameter passed into the task */
+                1, /* Task priority */
+                &displayUpdateHandle);
 
-  // Set up FreeRTOS task
-  vTaskStartScheduler();
+    // Set up FreeRTOS task
+    vTaskStartScheduler();
 }
 
 void loop()
 {
-  // This should never be reached
-  while (1)
-  {
-    Serial.println("Error: loop() should never be reached");
-    delay(1000);
-  }
+    // This should never be reached
+    while (1) {
+        Serial.println("Error: loop() should never be reached");
+        delay(1000);
+    }
 }
